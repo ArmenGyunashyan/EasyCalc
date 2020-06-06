1ad63d93e2749dae50087b28ccd596efaa02709d
Yggdrahl <denniskra159@web.de>
2020-06-06 11:43:03 +0200

    Changef static folder

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const path = require('path');
 
 // Die Hauptanwendung wird gestartet
 const app = express();
-
+/*
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
+*/
+//Statischen Ordner erstellen (root für die Webseite, nicht den Server)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Der Prot wird aus der Umgebungsvariable gelesen (wärend der Entwicklung ist er 5000 (Development))
 const PORT = process.env.PORT || 5000;
 // Der Prot wird festgelegt
